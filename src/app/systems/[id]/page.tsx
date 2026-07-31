a4e13b825a83f51b0b42284985ded73e35923e20
rarebuffalo <info.krishnasingh.codes@gmail.com>
2026-08-01 02:34:12 +0530

    remove unused React hook imports in systems page.tsx

--- src/app/systems/[id]/page.tsx
+++ src/app/systems/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, use } from "react";
+import React, { use } from "react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { portfolioSystems } from "@/data/portfolioData";
